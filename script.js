e2290db8d1cde8a05bdcf918e5b4e113cf2bd31e
ali_khanix <khaniali568@gmail.com>
2024-01-10 10:58:12 +0330

    going for slideshow

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const barsIcon = document.getElementById("bars-icon");
 const multiplyIcon = document.getElementById("multiply-icon");
+const navList = document.getElementById("nav-list");
+
+barsIcon.addEventListener("click", () => {
+  console.log("bars icon clicked");
+  navList.classList.add("menu-active");
+});
+
+multiplyIcon.addEventListener("click", () => {
+  navList.classList.remove("menu-active");
+});
